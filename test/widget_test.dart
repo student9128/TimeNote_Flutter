42c1574b8b6d7052fd0d05344dc95a9a531fa544
student9128 <jingpengchn@163.com>
2025-12-30 17:32:22 +0800

    add: add test code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:timenote_flutter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(TimeNoteApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
